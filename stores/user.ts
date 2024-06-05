403de46defc1dd7818c1104bd534fadbc0f7daaf
Mendenga <mendenga.takougnadi@hotmail.com>
2024-06-05 14:26:23 +0200

    first commit

---
+++ stores/user.ts
@@ -0,0 +1,34 @@
+import { defineStore } from 'pinia'
+import type { Customer, User } from '~/types';
+
+export const useMyUserStore = defineStore("user", () => {
+    const user = ref();
+    const token = useCookie('MY COOKIES', {
+        maxAge: 60*60,
+    })
+    const setToken = (data?: string) => (token.value = data);
+    const setUser = (data?: any) => (user.value = data);
+
+    const signIn = async (data: any ) => {
+        try {
+            const res = await $fetch<User>("https://dummyjson.com/auth/login",{
+                method: 'POST',
+                body: data,
+            });
+            setToken(res.token);
+        } catch (error) {
+            setToken();
+            setUser();
+            console.log();
+        }
+    }
+    const fetchCustomer = async () => {
+    if (token.value) {
+        try {
+            const res = await $fetch<Customer>("https://dummyjson.com/users/1")
+        } catch (error) {
+            setUser();
+            console.log(error);
+        }
+    }
+})
